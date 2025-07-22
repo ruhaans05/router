f7de8ba260c8dce3ed18f54ce2a0f24e0dcb7122
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:24:47 -0400

    Create flights.jsx

---
+++ flights.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Flights() {
+  return (
+    <div>
+      <h2>Manage Flights</h2>
+      <p>Here you’ll be able to search, compare, and book flights. (Feature coming soon!)</p>
+    </div>
+  );
+}
