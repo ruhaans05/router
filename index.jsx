ee16730cf3f9a6747edc265ce27c08e6a1201579
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:25:48 -0400

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "./Router.css"; // or whatever your CSS is called
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
