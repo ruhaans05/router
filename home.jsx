4118b90ae763e4e544d52ed04feca87c1eeaae73
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:24:57 -0400

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Router</h1>
+      <p>This is your all-in-one US transportation comparison tool.</p>
+      <p>Use the navigation above to explore features!</p>
+    </div>
+  );
+}
