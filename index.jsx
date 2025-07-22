833dc8656c0889669ffcb76a7a129a91fb7d0064
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-22 17:55:06 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app";
-import "./router.css"; // or whatever your CSS is called
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './home.jsx';
+import About from './about.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );
