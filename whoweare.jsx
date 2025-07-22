770ca45eb6294c64fb82383f75cb33108f8aae13
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:27:14 -0400

    Create whoweare.jsx

---
+++ whoweare.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function WhoWeAre() {
+  return (
+    <div>
+      <h2>Who We Are</h2>
+      <p>
+        Router is built by a passionate team of travel hackers, data engineers, and design nerds. <br/>
+        (Add more personal/team info here!)
+      </p>
+    </div>
+  );
+}
