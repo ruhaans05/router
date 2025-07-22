fa483ea8d1bd6466eecee4dc01280e0a83486481
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:36:16 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import "./Router.css"; // or whatever your CSS is called
+import "./router.css"; // or whatever your CSS is called
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
