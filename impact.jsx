4a0c90febe281286ad5b97104cfafe4f0cda6ad1
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:25:09 -0400

    Create impact.jsx

---
+++ impact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Impact() {
+  return (
+    <div>
+      <h2>Impact</h2>
+      <p>
+        Router helps users save money and reduce their carbon footprint by making smarter travel decisions. <br/>
+        (Put real stats or stories here!)
+      </p>
+    </div>
+  );
+}
