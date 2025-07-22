544184fdab1b548c8ffc813c1a7f32483380642b
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-21 23:24:05 -0400

    Create about.jsx

---
+++ about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function About() {
+  return (
+    <div>
+      <h2>About Router</h2>
+      <p>
+        Router helps you compare driving, bus, train, and flight routes across the US for cost and efficiency.
+      </p>
+      <p>
+        Created for travelers who want smarter, cheaper journeys.
+      </p>
+    </div>
+  );
+}
